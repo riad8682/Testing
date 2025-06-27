8ddfa035ee41741f21095e1e59e46a56ec3a792c
Md Foysal Ali Riyad <mdfaysalaliriyad1000@gmail.com>
2025-06-27 15:12:08 +0600

    initial commit

---
+++ app/api/inngest/route1.js
@@ -0,0 +1,12 @@
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+// import { helloWorld } from "../../../inngest/functions";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+  ],
+});
